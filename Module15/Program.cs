cb236ee029fb44c1966b6f9783144bd39080e6e6
AreURdy4TheRock <dorkwingdock@gmail.com>
2023-05-16 23:27:08 +0300

    Добавьте файлы проекта.

---
+++ Module15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            // ???
+            var result = new List<string>();
+            
+            for(int i = 0; i < classes.Length;i++)
+            {
+                var s = result.Concat(classes[i].Students).ToList();
+                result = s;
+            }
+            return result.ToArray();
+        }
+
+    }
+}
